ee43873d71d1e7c50ecd763d878da3ec699b4945
KseniyaRa <39523629+KseniyaRa@users.noreply.github.com>
2019-02-11 10:45:43 +0300

    Добавила защиту от XSS

--- templates/index.php
+++ templates/index.php
@@ -32,12 +32,12 @@ require_once('functions.php');
                 <img src="<?=$item['image'];?>" width="350" height="260" alt="">                    
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?=$item['category'];?></span>                    
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title'];?></a></h3>
+                <span class="lot__category"><?=htmlspecialchars($item['category']);?></span>                    
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['title']);?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
-                        <span class="lot__amount"><?=formatSumRub($item['price']);?></span>
-                        <span class="lot__cost"><?=formatSumRub($item['price']);?></span>
+                        <span class="lot__amount"><?=htmlspecialchars(formatSumRub($item['price']));?></span>
+                        <span class="lot__cost"><?=htmlspecialchars(formatSumRub($item['price']));?></span>
                     </div>
                     <div class="lot__timer timer">
                         12:23
