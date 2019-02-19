c593043723e788b6356e0189c1395a1f733d0de1
KseniyaRa <39523629+KseniyaRa@users.noreply.github.com>
2019-02-19 10:24:06 +0300

    Чтение и показ записей из MySQL 1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $user_name = 'Ксения';
 
 require_once('functions.php');
 require_once('data.php');
+require_once('init.php');
 
 $page_content = include_template('index.php', [ 
     'promo' => $promo,
@@ -22,6 +23,16 @@ $layout_content = include_template('layout.php', [
 ]);
 
 print($layout_content);
+
+//получаем открытые лоты
+$lots_categories = get_mysql_data($con, 'SELECT * FROM category', []);
+
+$lots_sql_query = 'SELECT lot.name, initial_price, image, /*цена*/ c.name 
+FROM lot
+JOIN category c
+ON lot.category_id = c.id
+ORDER BY lot.date DESC';
+
 ?>
 
 
--- init.php
+++ init.php
@@ -1,9 +1,11 @@
 <?php
+require_once('functions.php');
+
 $con = mysqli_connect("localhost", "root", "", "yeticave");
+
+mysqli_set_charset($con, "utf8");
+
 if ($con == false) {
    print("Ошибка подключения: ". mysqli_connect_error());
 }
-else {
-   print("Соединение установлено");
-   // выполнение запросов
 ?>
