ee9511b2f50ffa17a3e6b104b13728aeacbce627
KseniyaRa <39523629+KseniyaRa@users.noreply.github.com>
2019-03-01 14:38:32 +0300

    Работа со строкой запроса 7

--- lot.php
+++ lot.php
@@ -5,38 +5,42 @@ require_once('init.php');
 
 //проверяем существует ли id
 if (isset($_GET['id'])) {
-    $id = mysqli_real_escape_string($con, $_GET['id']);
-    $lot_id = intval($_GET['id']);
-    $sql_lot = "SELECT lot.name AS title, date, description, image, initial_price, completion_date, step_rate, author, winner, c.name 
-    FROM lot 
-    JOIN category c
-    ON lot.category_id = c.id WHERE id = '?'";
-    $lot = db_get_prepare_stmt($con, $sql_lot, [$lot_id]);
-        
-    $sql = 'SELECT `id`, `name` FROM category';
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    }
+$lot_id = mysqli_real_escape_string($con, $_GET['id']);
+$sql_lot = "SELECT lot.name AS title, `date`, discription, image, initial_price, completion_date, step_rate, author, winner, c.name 
+FROM lot 
+JOIN category c
+ON lot.category_id = c.id WHERE lot.id = ?";
+
+$stmt = db_get_prepare_stmt($con, $sql_lot, [$lot_id]);
+mysqli_stmt_execute($stmt);//Выполняет подготовленный запрос
+$res = mysqli_stmt_get_result($stmt);//Получает результат из подготовленного запроса
+
+$lot = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
+
+$sql = 'SELECT `id`, `name` FROM category';
+$result = mysqli_query($con, $sql);
+if ($result) {
+$categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
 }
 else {
-    header('Location: 404');
+header( "HTTP/1.1 404 Not Found" );
 }
 
 //отображаем список категорий
 $footer_content = include_template('footer.php', [
-    'promo' => $categories]
+'promo' => $categories]
 );
 
 // "собираем" всю станицу с лотом
 $lot_content = include_template('lot.php', [
-    'footer' => $footer_content,
-    'title' => $lot_title,
-    'item' => $lot //в шаблоне выводится как item поэтому так 
+'footer' => $footer_content,
+'title' => $lot_title,
+'item' => $lot[0] 
 ]);
 
 print($lot_content);
-
 
 /*if (!$con) {
     $error = mysqli_connect_error();
